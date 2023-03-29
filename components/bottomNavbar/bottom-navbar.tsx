f2b64c2966cb7e39213a03718c0e56a52ee8ba2d
Valerie Yew <48005126+valerieyew@users.noreply.github.com>
2023-03-29 13:50:35 +0800

    created bottom navbar component

---
+++ components/bottomNavbar/bottom-navbar.tsx
@@ -0,0 +1,60 @@
+import styles from './bottom-navbar.module.css';
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import book from '../../public/images/navbar-book.png'
+import bookSelected from '../../public/images/navbar-book-selected.png'
+import home from '../../public/images/navbar-home.png'
+import homeSelected from '../../public/images/navbar-home-selected.png'
+import profile from '../../public/images/navbar-profile.png'
+import profileSelected from '../../public/images/navbar-profile-selected.png'
+
+interface props {
+    currentPage?: string;
+    userId?: number;
+}
+
+const BottomNavbar:React.FC<props> = (props) => {
+
+    const router = useRouter();
+
+    const handleNavItemClick = (pageName: string) => {
+        // router.push(`/users/${pageName}/${props.userId}`); // adjust this code when we confirm the urls
+    };
+
+    return (
+        <footer className={styles.navbar}>
+            <div className={styles.iconContainer}>
+                <a href="#" onClick={() => handleNavItemClick('home')}>
+                    {props.currentPage !== 'home' && (
+                        <Image alt="homepage navbar icon" src={home} width="600" height="300" />
+                    )}
+                    {props.currentPage === 'home' && (
+                        <Image alt="homepage navbar icon" src={homeSelected} width="600" height="300" />
+                    )}
+                </a>
+            </div>
+            <div className={styles.iconContainer}>
+                <a href="#" onClick={() => handleNavItemClick('book')}>
+                    {props.currentPage !== 'book' && (
+                        <Image alt="booking page navbar icon" src={book} width="600" height="300" />
+                    )}
+                    {props.currentPage === 'book' && (
+                        <Image alt="booking page navbar icon" src={bookSelected} width="600" height="300" />
+                    )}
+                </a>
+            </div>
+            <div className={styles.iconContainer}>
+                <a href="#" onClick={() => handleNavItemClick('profile')}>
+                    {props.currentPage !== 'profile' && (
+                        <Image alt="profile page navbar icon" src={profile} width="600" height="300" />
+                    )}
+                    {props.currentPage === 'profile' && (
+                        <Image alt="profile page navbar icon" src={profileSelected} width="600" height="300" />
+                    )}
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default BottomNavbar;
