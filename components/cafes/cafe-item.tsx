baa014264a892b49739f0bcb1b2ec6d1b92bc55b
Bacon pancake <baconpancake002@egmail.com>
2023-04-03 13:18:36 +0800

    Profile page on construction

--- components/cafes/cafe-item.tsx
+++ components/cafes/cafe-item.tsx
@@ -1,5 +1,8 @@
 import styles from './cafe-item.module.css';
 import Button from '../ui/button';
+import Image from 'next/image';
+import Booking from '../../public/images/booking-image.png'
+import CafeBadge from './cafe-badge';
 interface props {
     className?: string;
     isOpen: boolean;
@@ -9,15 +12,24 @@ interface props {
 
 const CafeItem:React.FC<props> = (props) => {
     return (
-        <div className={styles.card} >
-            <img src="" alt="" />
-            <h3>Star</h3>
-            <h1>Name</h1>
-            <h2>time</h2>
-            <h2>Distance</h2>
-            <div>
-                <Button btntype="primary">Click</Button>
-                <Button btntype='secondary'>Click</Button>
+        <div className={styles.card} style={{ backgroundImage: `url(${Booking.src})` }}>
+            <div className={styles.titleContainer} >
+                <h3 className={styles.title}>Home Coffee Roasters</h3>
+            </div>
+            <div className={styles.badge}>
+                <CafeBadge />
+                <h3 className={styles.badgeText}>2/2</h3>
+            </div>
+            <div className={styles.headingContainer} >
+                <div className={styles.leftHeading}>
+                    <h3 className={styles.topleftHeadingText}>1 for 1 drinks </h3>
+                    <h3 className={styles.bottomleftHeadingText}>Any drinks from their regular menu</h3>
+                </div>
+                <div className={styles.rightHeading}>
+                    <div className={styles.redeem}>
+                        <h3 className={styles.rightHeadingText}>Redeemed</h3>
+                    </div>
+                </div>
             </div>
         </div>
     );
