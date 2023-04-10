acc1667dff4ea189d3bad9223324efc1052e8548
Valerie Yew <48005126+valerieyew@users.noreply.github.com>
2023-04-10 17:41:01 +0800

    fixed top banner and updated bookings page

--- pages/bookings/index.tsx
+++ pages/bookings/index.tsx
@@ -1,18 +1,26 @@
-import Button from '@/components/ui/button';
-import Input from '@/components/ui/input';
 import styles from '../../styles/Bookings.module.css';
 import CafeItem from '@/components/cafes/cafe-item';
 import BottomNavbar from '@/components/bottomNavbar/bottom-navbar';
 import TopBanner from '@/components/topBanner/top-banner';
+import BookingsBar from '@/components/users/bookings-bar';
 import { LinearProgress } from '@mui/material';
 
 export default function BookingsPage() {
     return (
         <div className={styles.container}>
             <TopBanner currentPage='book' />
-            <div className={styles.profileContainer}>
-                <LinearProgress variant="determinate" value={50} />
-                <CafeItem isOpen />
+            <div className={styles.bookingContainer}>
+                <BookingsBar />
+                <h1 className={styles.title}>
+                    My Booking List
+                </h1>
+                <div className={styles.cafeContainer} >
+                    <CafeItem isOpen />
+                </div>
+                
+                <h1 className={styles.title}>
+                    Need to cancel?
+                </h1>
             </div>
             <BottomNavbar currentPage="book" />
         </div>
