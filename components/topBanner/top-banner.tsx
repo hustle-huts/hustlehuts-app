7270e5e366b97b0bd940b39fd66314e9c975c849
Carina Tam <carinatam6@gmail.com>
2023-05-09 19:30:32 -0400

    MUI refactor: top banner

--- components/topBanner/top-banner.tsx
+++ components/topBanner/top-banner.tsx
@@ -1,48 +1,50 @@
-import styles from './top-banner.module.css';
-import Image from 'next/image'
-import cart from '../../public/images/cart.svg'
-import leftEclipse from '../../public/images/top-banner-left-eclipse.png'
-import middleBottomEclipse from '../../public/images/top-banner-middle-bottom-eclipse.svg'
-import middleTopEclipse from '../../public/images/top-banner-middle-top-eclipse.svg'
-import rightEclipse from '../../public/images/top-banner-right-eclipse.svg'
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import { AppBar, Toolbar, Typography, Badge } from "@mui/material";
+import { useEffect, useState } from "react";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
 
 interface props {
-    currentPage?: string;
-    userId?: number;
+  currentPage?: string;
+  userId?: number;
 }
 
-const TopBanner:React.FC<props> = (props) => {
+const TopBanner: React.FC<props> = (props) => {
+  const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(props.currentPage || "home");
 
-    const router = useRouter();
+  useEffect(() => {
+    setCurrentPage(props.currentPage || "home");
+  }, [props.currentPage])
 
-    const handleNavItemClick = () => {
-        // router.push(`/bookings/${props.userId}`); // adjust this code when we confirm the urls
-    };
+  const handleNavItemClick = () => {
+    // router.push(`/bookings/${props.userId}`); // adjust this code when we confirm the urls
+  };
 
-    return (
-        <div className={styles.background}>
-            <div className={styles.header}>
-                {props.currentPage === 'home' && (
-                    <p>Home</p>
-                )}
-                {props.currentPage === 'book' && (
-                    <p>Bookings</p>
-                )}
-                {props.currentPage === 'profile' && (
-                    <p>Profile</p>
-                )}
-            </div>
-            <a href="#" onClick={() => handleNavItemClick()}>
-                <Image className={styles.cart} alt="Cart button" src={cart} />
-            </a>
-            <Image className={styles.leftEclipse} alt="Background eclipse design" src={leftEclipse}/>
-            <Image className={styles.middleTopEclipse} alt="Background eclipse design" src={middleTopEclipse} />
-            <Image className={styles.middleBottomEclipse} alt="Background eclipse design" src={middleBottomEclipse} />
-            <Image className={styles.middleLeftEclipse} alt="Background eclipse design" src={middleTopEclipse} />
-            <Image className={styles.rightEclipse} alt="Background eclipse design" src={rightEclipse} />
-        </div>
-    );
-}
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        background: "linear-gradient(176.54deg, #6D5747 -11.89%, #B88151 64.46%)",
+        root: {
+          flexGrow: 1,
+        },
+        appbar: {
+          alignItems: "center",
+        },
+      }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" align="center" sx={{ flexGrow: 1, fontWeight: "800", fontSize: "1.65rem" }}>
+          {currentPage === "home" && "Home"}
+          {currentPage === "book" && "Bookings"}
+          {currentPage === "profile" && "Profile"}
+        </Typography>
+        <Badge color="warning" variant="dot">
+          <ShoppingCartOutlinedIcon sx={{ color: "#FFFEFF" }} />
+        </Badge>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default TopBanner;
