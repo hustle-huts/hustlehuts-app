007840a60a1e0f08effd28f3521132b2ce28ba28
jared <jach2355@gmail.com>
2023-05-09 17:12:21 +0800

    google oauth update client id

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 export default function SignUp() {
     return (
         <>
-        <GoogleOAuthProvider clientId="554539700276-7ofmtmaendohcdss79l752c1e25leb7e.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId="https://554539700276-7ofmtmaendohcdss79l752c1e25leb7e.apps.googleusercontent.com">
             <Image alt="" src={Logo} style={{ width: '50%', height: 'auto', margin:'40px 0px' }} width="600" height="300" />
             <LoginForm></LoginForm>
         </GoogleOAuthProvider>
