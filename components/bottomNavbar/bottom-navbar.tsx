6d32bc4f28e2114e589be6efa1466f3efe4c5a38
Carina Tam <carinatam6@gmail.com>
2023-05-09 18:46:38 -0400

    Refactor bottom navbar to use MUI

--- components/bottomNavbar/bottom-navbar.tsx
+++ components/bottomNavbar/bottom-navbar.tsx
@@ -1,60 +1,78 @@
-import styles from './bottom-navbar.module.css';
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import book from '../../public/images/navbar-book.svg'
-import bookSelected from '../../public/images/navbar-book-selected.svg'
-import home from '../../public/images/navbar-home.svg'
-import homeSelected from '../../public/images/navbar-home-selected.svg'
-import profile from '../../public/images/navbar-profile.svg'
-import profileSelected from '../../public/images/navbar-profile-selected.svg'
+import { useRouter } from "next/router";
+import { BottomNavigation, BottomNavigationAction, Typography } from "@mui/material";
+import { useState } from "react";
+import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import StoreIcon from "@mui/icons-material/Store";
+import StoreOutlinedIcon from "@mui/icons-material/StoreOutlined";
+import HomeIcon from "@mui/icons-material/Home";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 
 interface props {
-    currentPage?: string;
-    userId?: number;
+  currentPage?: string;
+  userId?: number;
 }
 
-const BottomNavbar:React.FC<props> = (props) => {
+const BottomNavbar: React.FC<props> = (props) => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const handleNavItemClick = (pageName: string) => {
+    // router.push(`/users/${pageName}/${props.userId}`); // adjust this code when we confirm the urls
+  };
 
-    const handleNavItemClick = (pageName: string) => {
-        // router.push(`/users/${pageName}/${props.userId}`); // adjust this code when we confirm the urls
-    };
+  const [currentPage, setCurrentPage] = useState(props.currentPage || "home");
+  const selectedColor = "#FFFEFF";
+  const unselectedColor = "#E2CCB9";
 
-    return (
-        <footer className={styles.navbar}>
-            <div className={styles.iconContainer}>
-                <a href="#" onClick={() => handleNavItemClick('home')}>
-                    {props.currentPage !== 'home' && (
-                        <Image alt="homepage navbar icon" src={home} width="600" height="300" />
-                    )}
-                    {props.currentPage === 'home' && (
-                        <Image alt="homepage navbar icon" src={homeSelected} width="600" height="300" />
-                    )}
-                </a>
-            </div>
-            <div className={styles.iconContainer}>
-                <a href="#" onClick={() => handleNavItemClick('book')}>
-                    {props.currentPage !== 'book' && (
-                        <Image alt="booking page navbar icon" src={book} width="600" height="300" />
-                    )}
-                    {props.currentPage === 'book' && (
-                        <Image alt="booking page navbar icon" src={bookSelected} width="600" height="300" />
-                    )}
-                </a>
-            </div>
-            <div className={styles.iconContainer}>
-                <a href="#" onClick={() => handleNavItemClick('profile')}>
-                    {props.currentPage !== 'profile' && (
-                        <Image alt="profile page navbar icon" src={profile} width="600" height="300" />
-                    )}
-                    {props.currentPage === 'profile' && (
-                        <Image alt="profile page navbar icon" src={profileSelected} width="600" height="300" />
-                    )}
-                </a>
-            </div>
-        </footer>
-    );
-}
+  return (
+    <BottomNavigation
+      showLabels
+      value={props.currentPage}
+      onChange={(_, newValue) => {
+        setCurrentPage(newValue);
+      }}
+      sx={{
+        background: "linear-gradient(176.54deg, #6D5747 -11.89%, #B88151 64.46%)",
+        position: "fixed",
+        bottom: 0,
+        width: 1.0,
+        padding: "0.5rem 0",
+      }}>
+      <BottomNavigationAction
+        label={
+          <Typography variant="body2" sx={{ color: currentPage === "home" ? selectedColor : unselectedColor }}>
+            Home
+          </Typography>
+        }
+        value="home"
+        icon={currentPage === "home" ? <HomeIcon sx={{ color: selectedColor }} /> : <HomeOutlinedIcon sx={{ color: unselectedColor }} />}
+      />
+      <BottomNavigationAction
+        label={
+          <Typography variant="body2" sx={{ color: currentPage === "book" ? selectedColor : unselectedColor }}>
+            Bookings
+          </Typography>
+        }
+        value="book"
+        icon={currentPage === "book" ? <StoreIcon sx={{ color: selectedColor }} /> : <StoreOutlinedIcon sx={{ color: unselectedColor }} />}
+      />
+      <BottomNavigationAction
+        label={
+          <Typography variant="body2" sx={{ color: currentPage === "profile" ? selectedColor : unselectedColor }}>
+            Profile
+          </Typography>
+        }
+        value="profile"
+        icon={
+          currentPage === "profile" ? (
+            <PersonRoundedIcon sx={{ color: selectedColor }} />
+          ) : (
+            <PersonOutlineOutlinedIcon sx={{ color: unselectedColor }} />
+          )
+        }
+      />
+    </BottomNavigation>
+  );
+};
 
 export default BottomNavbar;
