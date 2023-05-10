f0678d1a16db6b4663640a270bcfbe80c0236383
Carina Tam <carinatam6@gmail.com>
2023-05-10 00:25:37 -0400

    MUI refactor: login 1 work

--- components/users/login-form.tsx
+++ components/users/login-form.tsx
@@ -1,52 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import GoogleIcon from '../icons/google_icon';
-import FacebookIcon from '../icons/facebook-icon';
-import OutlookIcon from '../icons/outlook-icon';
-import Button from '../ui/button';
-import Input from '../ui/input';
-import styles from './login-form.module.css';
-import { useGoogleLogin, TokenResponse } from '@react-oauth/google';
-
+import React, { useEffect, useState } from "react";
+import GoogleIcon from "../icons/google_icon";
+import FacebookIcon from "../icons/facebook-icon";
+import OutlookIcon from "../icons/outlook-icon";
+import Button from "../ui/button";
+import Input from "../ui/input";
+import styles from "./login-form.module.css";
+import { useGoogleLogin, TokenResponse } from "@react-oauth/google";
 
 interface Values {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
-// This is for the main login/signup page with Google, FB and Outlook sign in 
+// This is for the main login/signup page with Google, FB and Outlook sign in
 export default function LoginForm() {
-    const [tokenResponse, setTokenResponse] = useState<TokenResponse | null>();
-    const [user, setUser] = useState<any>(null);
+  const [tokenResponse, setTokenResponse] = useState<TokenResponse | null>();
+  const [user, setUser] = useState<any>(null);
 
-    const login = useGoogleLogin({
-        onSuccess: tokenResponse => console.log(tokenResponse),
-      });
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => console.log(tokenResponse),
+  });
 
+  return (
+    <form className={styles.form}>
+      <div className={styles.form_row}>
+        <Input error="Only alphabetical characters allowed!" isRequired={true} label="Enter Your Email to Begin"></Input>
+        {/* <div className="invalid-feedback">{errors.firstName?.message}</div> */}
+      </div>
+      <div className={styles.btn_groups}>
+        <Button>Login</Button>
+        <Button btntype="secondary">Sign Up</Button>
+        <span className={styles.line}>
+          <div style={{ flex: 1, height: "1px", backgroundColor: "black", width: "40%" }} />
+          <h3>OR</h3>
+          <div style={{ flex: 1, height: "1px", backgroundColor: "black" }} />
+        </span>
 
-    return (
-        <form className={styles.form}>
-            <div className={styles.form_row}>
-                <Input error="Only alphabetical characters allowed!" isRequired={true} label='Enter Your Email to Begin'></Input>
-                    {/* <div className="invalid-feedback">{errors.firstName?.message}</div> */}
-                
-            </div>
-            <div className={styles.btn_groups}>
-                <Button>Login</Button>
-                <Button btntype='secondary'>Sign Up</Button>
-                <span className={styles.line}>
-                    <div style={{flex: 1, height: '1px', backgroundColor: 'black', width:'40%'}} />
-                    <h3>OR</h3>
-                    <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
-                </span>
-
-
-                <Button clickEvent={() => login()} icon={<GoogleIcon />} btntype="outline">
-                    Sign in with Google
-                </Button>
-                <Button icon={<FacebookIcon/>} btntype="outline">Sign Up with Facebook</Button>
-                <Button icon={<OutlookIcon/>} btntype="outline">Sign Up with Outlook</Button>
-            </div>
-           
-        </form>
-    );
-  };
+        <Button clickEvent={() => login()} icon={<GoogleIcon />} btntype="outline">
+          Sign in with Google
+        </Button>
+        <Button icon={<FacebookIcon />} btntype="outline">
+          Sign Up with Facebook
+        </Button>
+        <Button icon={<OutlookIcon />} btntype="outline">
+          Sign Up with Outlook
+        </Button>
+      </div>
+    </form>
+  );
+}
