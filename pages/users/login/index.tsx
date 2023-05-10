9fa01494e416ca7fd15b801bcfa48ce82cbcb9b7
Carina Tam <carinatam6@gmail.com>
2023-05-10 17:32:21 -0400

    MUI refactor: login 2

--- pages/users/login/index.tsx
+++ pages/users/login/index.tsx
@@ -1,20 +1,86 @@
-import Button from '@/components/ui/button';
-import Input from '@/components/ui/input';
-import LoginHeader from '@/components/users/login-header';
-import styles from '../../../styles/Login.module.css';
+import HeaderImage from "../../../public/images/LoginHeader.png";
+import { Stack, Box, Typography, TextField, IconButton, InputAdornment, Button } from "@mui/material";
+import { useState } from "react";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+
 export default function LogInPage() {
+  const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
     return (
-        <div className={styles.container}>
-            <LoginHeader/>
-            <h1 className={styles.title}>Log In</h1>
-            <form className={styles.form}>
-                <div className={styles.form_row}>
-                    <Input type="password" label='Enter Password' isRequired={true}></Input>
-                </div>
-                <div className={styles.btn_groups}>
-                    <Button>Log In</Button>
-                </div>
-            </form>
-        </div>
+    <>
+      <Stack spacing={2} sx={{ justifyContent: "center" }}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "50vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            top: 0,
+          }}>
+          <img
+            src={HeaderImage.src}
+            style={{
+              width: "100%",
+              height: "50vh",
+              objectFit: "cover",
+              objectPosition: "50% 100%",
+            }}
+          />
+        </Box>
+
+        <Typography
+          variant="h3"
+          sx={{
+            fontWeight: 700,
+            textAlign: "center",
+          }}>
+          Log In
+        </Typography>
+        <Box sx={{ display: "flex", justifyContent: "center", pt: "5vh" }}>
+          <TextField
+            sx={{
+              width: "70%",
+              "& .MuiFormLabel-root": {
+                fontWeight: 500,
+                color: "#6D5747",
+              },
+            }}
+            label={"Enter Password"}
+            variant="outlined"
+            InputLabelProps={{ shrink: true }}
+            required
+            value={password}
+            type={showPassword ? "text" : "password"}
+            onChange={(e) => setPassword(e.target.value)}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                    {showPassword ? <VisibilityOffOutlinedIcon /> : <VisibilityOutlinedIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center", pt: "5vh" }}>
+          <Button
+            variant="contained"
+            sx={{
+              background: "linear-gradient(176.54deg, #6D5747 -11.89%, #B88151 64.46%)",
+              fontWeight: 700,
+              borderRadius: "12px",
+              boxShadow: "0px 4px 40px rgba(160, 116, 78, 0.18)",
+              width: "70%",
+              height: "3rem",
+            }}>
+            Login
+          </Button>
+        </Box>
+      </Stack>
+    </>
     );
 }
