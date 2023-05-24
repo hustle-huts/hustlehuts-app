821d612e32fc18f5463c6d137b99aeb03793efbf
Koh Vinleon <kohvinleon@gmail.com>
2023-05-24 23:10:34 +0800

    Change to axios

--- services/login.service.ts
+++ services/login.service.ts
@@ -1,23 +1,38 @@
-const BASE_URL = `${process.env.API_URL}/api/auth`;
+import axios_instance from "./axios";
 
-export const loginApi = async (email: string, password: string) => {
-  const response = await fetch(`${BASE_URL}/login`, {
-    method: "POST",
-    body: JSON.stringify({ email, password }),
-  });
-  const data = await response.json();
-  return data;
+const PREFIX_URL = `/api/auth`;
+
+const loginApi = async (email: string, password: string) => {
+  try {
+    const response = await axios_instance.post(`${PREFIX_URL}/login`, {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const registerApi = async (
+const registerApi = async (
+  first_name: string,
+  last_name: string,
   email: string,
   password: string,
-  name: string
+  telegram_handle?: string
 ) => {
-  const response = await fetch(`${BASE_URL}/register`, {
-    method: "POST",
-    body: JSON.stringify({ email, password, name }),
-  });
-  const data = await response.json();
-  return data;
+  try {
+    const response = await axios_instance.post(`${PREFIX_URL}`, {
+      first_name,
+      last_name,
+      email,
+      password,
+      telegram_handle,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+export { loginApi, registerApi };
