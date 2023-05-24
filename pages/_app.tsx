0fcf7d013adffa14e765d2bb8918e7af459a8875
Koh Vinleon <kohvinleon@gmail.com>
2023-05-24 21:29:39 +0800

    Add .env sample, max-width and Recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Poppins } from '@next/font/google'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "@next/font/google";
+import { RecoilRoot } from "recoil";
 const font = Poppins({
-  weight: ['400', '700', '500'],
-  subsets: ['latin'],
-})
+  weight: ["400", "700", "500"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <main className={font.className}>
-    <Component {...pageProps} />
-  </main>
-  
-)}
+    <RecoilRoot>
+      <main className={font.className}>
+        <Component {...pageProps} />
+      </main>
+    </RecoilRoot>
+  );
+}
